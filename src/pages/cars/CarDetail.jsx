df2a43094aef8f01615d576d12c8973b9e2b8de7
Vincent <vleclerc@inow.fr>
2021-07-28 10:21:03 +0200

    Object.assign

--- src/pages/cars/CarDetail.jsx
+++ src/pages/cars/CarDetail.jsx
@@ -6,23 +6,43 @@ export default class CarDetail extends Component {
 
     servCar = new CarsService();
     servBrand = new BrandsService();
+    //1
+    /*state = { car: null, brand: null };
 
+    componentDidMount() {
+        const id = this.props.match.params.id;
+        this.servCar.getCarById(id).then((data) => {
+            this.setState({ car: data });
+
+            this.servBrand.getBrandById(data.brandID).then((dataBrand) => {
+                this.setState({ brand: dataBrand });
+            });
+        });
+    }*/
+
+    //2 
     state = { car: null };
 
     componentDidMount() {
         const id = this.props.match.params.id;
         this.servCar.getCarById(id).then((data) => {
             this.setState({ car: data });
+
+            this.servBrand.getBrandById(data.brandID).then((dataBrand) => {
+                this.setState({ car: Object.assign(this.state.car, { brandName: dataBrand.name }) });
+            });
         });
     }
 
     render() {
+        console.log(this.state);
         return (
             <div>
                 <h1>Voiture {this.state.car?.model}</h1>
                 <p>{this.state.car?.price} €</p>
                 <p>{this.state.car?.dateOfCirculation}</p>
-                <p>{this.state.car?.brandID}</p>
+                <p>{/*1 this.state.brand?.name*/}</p>
+                <p>{this.state.car?.brandName}</p>
             </div>
         )
     }
